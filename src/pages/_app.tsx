56b81a0aad5ea4736cfd0f416dd5ffdc3a719d15
GuiiMaranhao <guiilherme.maranhao98@gmail.com>
2021-02-10 21:05:05 -0300

    chore: GlobalStyle styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import App from 'next/app';
+import React from 'react';
+import GlobalStyle from '../styles/GlobalStyle';
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
-  }
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
