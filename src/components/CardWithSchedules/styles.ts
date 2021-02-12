f343bab9628eb60562dcf952852d64e77bb0636a
GuiiMaranhao <guiilherme.maranhao98@gmail.com>
2021-02-12 13:07:50 -0300

    Component CardWithSchedules

---
+++ src/components/CardWithSchedules/styles.ts
@@ -0,0 +1,105 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.div`
+ & + div {
+    margin-top: 16px;
+  }
+`
+export const CardContent = styled.div`
+  background: #fff;
+  border-radius: 4px;
+  padding: 24px 48px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 24px 16px;
+  }
+`
+
+export const CardDescription = styled.div`
+
+  @media (max-width: 768px) {
+    display: flex;
+    gap: 32px;
+    margin-bottom: 24px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+
+
+    span {
+      color: #A8A8B3;
+      text-transform: uppercase;
+    }
+
+    strong {
+      color: #3D3D4D;
+      font-size: 24px;
+      line-height: 32px;
+      font-weight: 600;
+    }
+  }
+
+  div:last-child {
+    margin-top: 40px;
+
+    @media (max-width: 768px) {
+      margin-top: 0px;
+    }
+
+    strong {
+      color: #00984A;
+    }
+  }
+`
+
+export const CardFooter = styled.div`
+  background: #fff;
+  border-radius: 4px;
+  margin-top: 4px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 24px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+
+  span {
+    color: #A8A8B3;
+    text-transform: uppercase;
+    font-size: 12px;
+    line-height: 16px;
+
+    @media (max-width: 768px) {
+      margin-bottom: 16px;
+    }
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    strong {
+      line-height: 24px;
+      color: #3D3D4D;
+    }
+
+    svg {
+      margin: 0 32px;
+
+      @media (max-width: 768px) {
+        margin: 0 16px;
+      }
+    }
+  }
+`
+
