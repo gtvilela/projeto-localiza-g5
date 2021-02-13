7fa51b381252e570ff421b98c0a1bf9c11b8f0b4
Samuel Pietra <samuelpietra@outlook.com>
2021-02-13 13:16:32 -0300

    wip

--- src/components/global/Input/styles.ts
+++ src/components/global/Input/styles.ts
@@ -25,29 +25,58 @@ export const Container = styled.div<ContainerProps>`
     `}
 
   ${(props) =>
-    props.isFocused &&
-    css`
-      color: #00984a;
-      border-color: #00984a;
-      outline: none;
-      transition: all 500ms ease;
-    `}
+    props.isFocused
+      ? css`
+          color: #00984a;
+          border-color: #00984a;
+          outline: none;
+          transition: all 500ms ease;
+
+          svg {
+            color: #00984a;
+          }
+
+          input::-webkit-input-placeholder {
+            -webkit-transition: all 0.3s ease-in-out;
+            transition: all 0.3s ease-in-out;
+          }
+
+          input:valid::-webkit-input-placeholder {
+            font-size: 11px;
+            -webkit-transform: translateY(-8px);
+            transform: translateY(-8px);
+            visibility: visible !important;
+          }
+        `
+      : css`
+          input::-webkit-input-placeholder {
+            -webkit-transition: all 0.3s ease-in-out;
+            transition: all 0.3s ease-in-out;
+          }
+
+          input:valid::-webkit-input-placeholder {
+            font-size: 1em;
+            -webkit-transform: translateY(0px);
+            transform: translateY(0px);
+            visibility: visible !important;
+          }
+        `}
 
     div {
-      padding: 0 12px;
-      border-right: 1px solid #a8a8b3;
-      display: flex;
-      height: 60%;
-      align-items: center;
-      justify-content: center;
-      color: #a8a8b3;
+    padding: 0 12px;
+    border-right: 1px solid #a8a8b3;
+    display: flex;
+    height: 60%;
+    align-items: center;
+    justify-content: center;
+    color: #a8a8b3;
 
-      ${(props) =>
+    ${(props) =>
       props.isFilled &&
       css`
         color: #00984a;
       `}
-    }
+  }
 
   input {
     width: 100%;
@@ -55,7 +84,7 @@ export const Container = styled.div<ContainerProps>`
     border: 0;
     color: #000;
     outline: none;
-    padding: 0 8px;
+    padding: 0 16px;
 
     &::placeholder {
       color: #666360;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useRef } from 'react';
 
 import Header from '@components/global/Header/Header';
-import Card from '@components/global/Card';
+import Input from '@components/global/Input';
 
-import { Content, BoxCards } from '@styles/pages/dashboard';
+import { Content } from '@styles/pages/dashboard';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import { FiUser } from 'react-icons/fi';
 
 const Dashboard = () => {
+  const formRef = useRef<FormHandles>(null);
+
   return (
     <>
       <Header hidden={true} />
       <Content>
-        <BoxCards>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </BoxCards>
-     </Content>
+        <Form ref={formRef} onSubmit={() => {}}>
+          <Input name="name" icon={FiUser} placeholder="Usuário" />
+        </Form>
+      </Content>
     </>
   );
-}
+};
 
-export default Dashboard
+export default Dashboard;
