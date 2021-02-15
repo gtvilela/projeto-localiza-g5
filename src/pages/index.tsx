8b87f8736dfaf134b5846553e52de85c7680c94a
Samuel Pietra <samuelpietra@outlook.com>
2021-02-15 20:11:37 -0300

    remove alteracoes pagina index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,25 @@
-import React, { useRef } from 'react';
+import React from "react";
 
 import Header from '@components/global/Header/Header';
-import Input from '@components/global/Input';
+import Card from '@components/global/Card';
 
-import { Content } from '@styles/pages/dashboard';
-import { Form } from '@unform/web';
-import { FormHandles } from '@unform/core';
-import { FiUser } from 'react-icons/fi';
+import { Content, BoxCards } from '@styles/pages/dashboard';
 
 const Dashboard = () => {
-  const formRef = useRef<FormHandles>(null);
-
   return (
     <>
       <Header hidden={true} />
       <Content>
-        <Form ref={formRef} onSubmit={() => {}}>
-          <Input name="name" icon={FiUser} placeholder="Usuário" />
-        </Form>
-      </Content>
+        <BoxCards>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </BoxCards>
+     </Content>
     </>
   );
-};
+}
 
-export default Dashboard;
+export default Dashboard
