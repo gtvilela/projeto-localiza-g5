cefdb06c100abfe81ae68e104f62e9b135f405d6
GuiiMaranhao <guiilherme.maranhao98@gmail.com>
2021-02-15 21:44:50 -0300

    remover css inutil tab

--- src/components/global/Tab/styles.ts
+++ src/components/global/Tab/styles.ts
@@ -3,15 +3,3 @@ import styled from 'styled-components';
 export const TabContainer = styled.div`
   width: 100%;
 `;
-
-export const TabHeader = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-bottom: 1px solid black;
-`;
-
-export const TabHeaderItem = styled.button`
-  outline: none;
-  background: transparent;
-`;
