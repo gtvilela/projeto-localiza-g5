a4aefebf749866265c87eb3918ecc369d1c20482
GuiiMaranhao <guiilherme.maranhao98@gmail.com>
2021-02-16 00:42:31 -0300

    Alteração dashboard

--- src/styles/pages/dashboard.ts
+++ src/styles/pages/dashboard.ts
@@ -1,4 +1,46 @@
 import styled from 'styled-components';
+import { Form } from '@unform/web';
+
+import theme from '@styles/theme'
+
+export const Section = styled.div`
+  min-height: 264px;
+  background: url('/assets/header.jpg');
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.5);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: ${theme.colors.primaryDark};
+`;
+
+export const SectionContainer = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 48px 16px 48px;
+  color: #fff;
+
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  @media (max-width: 425px) {
+    flex-direction: column-reverse;
+  }
+
+`;
+export const TitleSection = styled.h1`
+  @media (max-width: 425px) {
+    display: none;
+  }
+`;
+
+export const FormCustom = styled(Form)`
+  max-width: 348px;
+
+  @media (max-width: 425px) {
+    max-width: 100%;
+  }
+`;
 
 export const Content = styled.div`
   width: 100%;
@@ -6,6 +48,32 @@ export const Content = styled.div`
   margin: 0 auto;
   padding: 48px 16px 24px;
 `
+
+export const HeaderPag = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 8px;
+  border-bottom: 1px solid ${theme.colors.textLighter};
+  margin-bottom: 18px;
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+  }
+
+  h2 {
+    color: ${theme.colors.text};
+    font-size: 24px;
+
+    @media (max-width: 425px) {
+      margin-bottom: 8px;
+    }
+  }
+
+  span {
+    color: ${theme.colors.textLighter}
+  }
+`;
 
 export const BoxCards = styled.div`
   display: flex;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,7 @@ const theme = {
     white: '#fff',
     green: '#f00',
     primary: '#00984A',
+    primaryDark: '#004A34',
     secondary: '#D3D800',
     backgroundApp: '#F2F2FA',
     text: '#3D3D4D',
