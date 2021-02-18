5570407f4f0e6eac22db200a9e8630cb82968635
Samuel Pietra <samuelpietra@outlook.com>
2021-02-18 15:29:10 -0300

    correcoes diversas na estrutura de pastas e arquivos

--- src/components/global/Dropdown/index.tsx
+++ src/components/global/Dropdown/index.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
+import Link from 'next/link';
+
 import { IPropsDropdown } from './types';
 import { DropdownContainer } from './styles';
-import Link from 'next/link';
-import {items} from './types/items';
+import { items } from './types/items';
 
 const Dropdown: FC<IPropsDropdown> = ({ isOpen }) => {
-
   return (
     <DropdownContainer>
       {isOpen && (
@@ -13,9 +13,9 @@ const Dropdown: FC<IPropsDropdown> = ({ isOpen }) => {
           {items.map((item) => (
             <li key={item.id}>
               <Link href={`/${item.rota.toLowerCase()}/1`} passHref>
-              <a>
-                <span>{item.value}</span>
-              </a>
+                <a>
+                  <span>{item.value}</span>
+                </a>
               </Link>
             </li>
           ))}
