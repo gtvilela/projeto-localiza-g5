95ffabe94ed879124d8c835b5a83e22136e25c39
GuiiMaranhao <guiilherme.maranhao98@gmail.com>
2021-02-17 22:51:21 -0300

    Novas funcionalidades component Button

--- src/components/global/Button/index.tsx
+++ src/components/global/Button/index.tsx
@@ -3,12 +3,15 @@ import React, { ButtonHTMLAttributes, FC } from 'react';
 import { ButtonContainer } from './styles';
 
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  color?: 'green' | 'yellow'
+  color?: 'green' | 'yellow' | 'red';
+  size?: 'large' | 'medium';
+  radius?: boolean;
+  fullwidth?: boolean;
 }
 
-const Button: FC<IButtonProps> = ({ type = 'button', color, children, ...rest }) => {
+const Button: FC<IButtonProps> = ({ type = 'button', size = 'large', color, radius, children, ...rest }) => {
   return (
-    <ButtonContainer type={type} color={color} {...rest}>
+    <ButtonContainer type={type} color={color} size={size} radius={radius} {...rest}>
       {children}
     </ButtonContainer>
   );
